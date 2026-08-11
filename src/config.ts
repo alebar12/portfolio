4ef214fb44dd84fa8c947249f66b992094ce41f5
Alessio Barducci <alessio.barducci@team.blue>
2026-08-11 15:56:11 +0200

    fix: fixed image reference

--- src/config.ts
+++ src/config.ts
@@ -25,7 +25,7 @@ export const siteConfig = {
         "Music streaming player for Android. 50k+ downloads on Google Play Store and Amazon Appstore. Built using external APIs from SoundCloud, Last FM, and Audius. Backend based on Java Spring Boot, hosted on Heroku. No longer actively maintained.",
       link: "https://www.amazon.com/Shuffly-Music-Song-Streaming-Player/dp/B01HGWUJK0",
       skills: ["Android", "Java", "Kotlin", "Music", "SoundCloud", "LastFM", "Audius"],
-      image: "/public/shufflymusic.png",
+      image: "/portfolio/shufflymusic.png",
     },
     {
       name: "Insane Soccer",
@@ -33,7 +33,7 @@ export const siteConfig = {
         "Open source arcade soccer game written in Typescript.",
       link: "https://github.com/alebar12/insane-soccer",
       skills: ["Typescript", "Games", "Open Source", "HTML5 Canvas"],
-      image: "/public/insanesoccer1.png",
+      image: "/portfolio/insanesoccer1.png",
     },
     {
       name: "Soccer Coins",
@@ -41,7 +41,7 @@ export const siteConfig = {
         "Arcade soccer game for Android. 500k+ downloads and 2.5k+ reviews on Google Play Store. No longer actively maintained.",
       link: "https://play.google.com/store/apps/details?id=it.soccerCoins",
       skills: ["Android", "Java", "Games"],
-      image: "/public/soccercoins.png",
+      image: "/portfolio/soccercoins.png",
     },
     {
       name: "Wear Assistant",
@@ -56,7 +56,7 @@ export const siteConfig = {
         "Puzzle game about light reflection for Android. Built using AndEngine. No longer actively maintained.",
       link: "https://www.amazon.com/FluiBex-Mirror-Challenge/dp/B018XYQWSI",
       skills: ["Android", "Java", "AndEngine", "Games"],
-      image: "/public/mirrorchallenge.png",
+      image: "/portfolio/mirrorChallenge3.jpg",
     },
     {
       name: "Guess the price",
